83a1a010c34853b46dccd74cb29c3272f9f565a9
Mateus Ito Silva <mateusito10@gmail.com>
2021-09-06 19:52:48 -0300

    🔨 | Nuxt - first commit

---
+++ nuxt.config.js
@@ -0,0 +1,117 @@
+// prettier-ignore
+export default {
+  /**
+   * |------------------------------------------------------------------------|
+   * | Target - JAM Stack                                                     |
+   * |------------------------------------------------------------------------|
+   */
+  target: 'static',
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | Global Head                                                            |
+   * |------------------------------------------------------------------------|
+   */
+  head: {
+    // Title -----------------------------------------------------------------|
+    title: process.env.SEO_TITLE,
+
+    // Meta Tags -------------------------------------------------------------|
+    meta: [
+      { charset: 'utf-8'                                                                   },
+      { name: 'viewport',         content: 'width=device-width, initial-scale=1'           },
+      { name: 'description',      content: process.env.SEO_DESCRIPTION, hid: 'description' },
+      { name: 'format-detection', content: 'telephone=no'                                  },
+			{ name: 'msapplication-TileColor', content: '#ffffff'                                },
+			{ name: 'theme-color',             content: '#000'                                   },
+
+      // SEO Tags ------------------------------------------------------------|
+      { name: 'title',                   content: process.env.SEO_TITLE                    },
+			{ name: 'description',             content: process.env.SEO_DESCRIPTION              },
+
+			// Open Graph / Facebook -----------------------------------------------|
+			{ property: 'og:type',             content: 'website'                                },
+			{ property: 'og:url',              content: process.env.SEO_PERMALINK                },
+			{ property: 'og:title',            content: process.env.SEO_TITLE                    },
+			{ property: 'og:description',      content: process.env.SEO_DESCRIPTION              },
+			{ property: 'og:image',            content: process.env.SEO_PREVIEW                  },
+
+			// Twitter --------------------------------------------------------------|
+			{ property: 'twitter:card',        content: 'summary_large_image'                    },
+			{ property: 'twitter:url',         content: process.env.SEO_PERMALINK                },
+			{ property: 'twitter:title',       content: process.env.SEO_TITLE                    },
+			{ property: 'twitter:description', content: process.env.SEO_DESCRIPTION              },
+			{ property: 'twitter:image',       content: process.env.SEO_PREVIEW                  },
+    ],
+
+    // Link Tags -------------------------------------------------------------|
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'                 },
+
+      // Google fonts --------------------------------------------------------|
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com'                 },
+			{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+    ],
+  },
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | Global CSS                                                             |
+   * |------------------------------------------------------------------------|
+   */
+  css: [],
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | Vue Plugins                                                            |
+   * |------------------------------------------------------------------------|
+   */
+  plugins: [],
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | Auto Import Components                                                 |
+   * |------------------------------------------------------------------------|
+   */
+  components: true,
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | Nuxt Modules for Development                                           |
+   * |------------------------------------------------------------------------|
+   */
+  buildModules: [
+    // TypeScript ------------------------------------------------------------|
+    '@nuxt/typescript-build',
+
+    // WindiCSS --------------------------------------------------------------|
+    'nuxt-windicss',
+
+    // DotEnv variables ------------------------------------------------------|
+    '@nuxtjs/dotenv',
+
+    // Composition API -------------------------------------------------------|
+    '@nuxtjs/composition-api/module',
+  ],
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | Nuxt Modules                                                           |
+   * |------------------------------------------------------------------------|
+   */
+  modules: [
+    // Progressive Web App ---------------------------------------------------|
+    '@nuxtjs/pwa',
+  ],
+
+  /**
+   * |------------------------------------------------------------------------|
+   * | PWA Configuration                                                      |
+   * |------------------------------------------------------------------------|
+   */
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent, Ref, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const name: Ref<string> = ref('Nuxt')
+
+    return { name }
+  },
+})
+</script>
+
+<template>
+  <div class="home__wrapper">
+    <h1 class="home__title">Hello, {{ name }}!</h1>
+  </div>
+</template>
+
+<style lang="postcss">
+.home__wrapper {
+  @apply h-screen;
+  @apply grid place-items-center;
+  @apply bg-dark-800;
+
+  .home__title {
+    @apply text-9xl text-transparent font-bold;
+    @apply bg-gradient-to-r bg-clip-text;
+    @apply from-blue-500 via-teal-500 to-green-400;
+  }
+}
+</style>
